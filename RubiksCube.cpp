3244225b4c262d655685089e8cdd4b1e79a47aa1
Nupur Chavan <chavannnupur57@gmail.com>
2024-07-06 12:42:51 +0530

    RubiksCube File Added

---
+++ RubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 06/07/2024.
+//
+
+#include "RubiksCube.h"
---
+++ RubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by HP on 06/07/2024.
+//
+
+#ifndef RUBIKSCUBE_H
+#define RUBIKSCUBE_H
+
+
+
+class RubiksCube {
+
+};
+
+
+
+#endif //RUBIKSCUBE_H
